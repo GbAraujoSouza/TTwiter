82ed21242af6bb9915459e783e7581ea463f8bc0
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-01-16 15:15:05 -0300

    Modularizar classe

---
+++ js/Post.js
@@ -0,0 +1,36 @@
+class Post {
+  constructor(imgPath, userImgPath, nomeUsuario, legenda) {
+    this.imgPath = imgPath;
+    this.userImgPath = userImgPath | "";
+    this.nomeUsuario = nomeUsuario
+    this.legenda = legenda;
+  }
+
+  constroiElemento() {
+    return `
+          <div class="post_header f_align_center">
+            <img src="${this.userImgPath}">
+            <span>${this.nomeUsuario}</span>
+          </div>
+          <div class="post_conteudo">
+            <div class="post_imagem">
+              <img src="${this.imgPath}" alt="Imagem do post">
+            </div>
+            <div class="post_btns espacado">
+              <span class="interact">
+                <iconify-icon icon="ant-design:like-outlined" width="32" height="32"></iconify-icon>
+                <iconify-icon icon="uiw:message" width="32" height="32"></iconify-icon>
+              </span>
+              <span class="like">
+                <iconify-icon icon="icon-park-outline:like" style="color: #db0031;" width="32" height="32"></iconify-icon>
+              </span>
+            </div>
+            <div class="legenda">
+              <p>${this.legenda}</p>
+            </div>
+          </div>
+        `
+  }
+}
+
+export default Post;
--- js/display-posts.js
+++ js/display-posts.js
@@ -1,37 +1,4 @@
-class Post {
-  constructor(imgPath, userImgPath,nomeUsuario, legenda) {
-    this.imgPath = imgPath;
-    this.userImgPath = userImgPath;
-    this.nomeUsuario = nomeUsuario
-    this.legenda = legenda;
-  }
-
-  constroiElemento() {
-    return `
-          <div class="post_header f_align_center">
-            <img src="${this.userImgPath}">
-            <span>${this.nomeUsuario}</span>
-          </div>
-          <div class="post_conteudo">
-            <div class="post_imagem">
-              <img src="${this.imgPath}" alt="Imagem do post">
-            </div>
-            <div class="post_btns espacado">
-              <span class="interact">
-                <iconify-icon icon="ant-design:like-outlined" width="32" height="32"></iconify-icon>
-                <iconify-icon icon="uiw:message" width="32" height="32"></iconify-icon>
-              </span>
-              <span class="like">
-                <iconify-icon icon="icon-park-outline:like" style="color: #db0031;" width="32" height="32"></iconify-icon>
-              </span>
-            </div>
-            <div class="legenda">
-              <p>${this.legenda}</p>
-            </div>
-          </div>
-        `
-  }
-}
+import Post from "./Post.js";
 
 const postContainer = document.querySelector(".post_cards");
 
@@ -39,7 +6,7 @@ const postContainer = document.querySelector(".post_cards");
 const postsArray = [
   new Post("./img/post_image1.jpg", "./img/profile_image.jpg", "HIDEO_KOJIMA", "With Al Pacino san."),
   new Post("./img/post_image2.jpg", "./img/profile_image.jpg", "HIDEO_KOJIMA", "With Norman."),
-  new Post("./img/post_image3.jpg", "./img/profile_image.jpg", "HIDEO_KOJIMA", "Our Keanu visited Kojima Productions."),
+  new Post("./img/post_image3.jpg", "HIDEO_KOJIMA", "Our Keanu visited Kojima Productions."),
 ];
 
 const displayPosts = (listaDePosts) => {
