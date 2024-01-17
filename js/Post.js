3259bbf86df699142ad3e3e8886884dced3e291a
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2024-01-17 19:28:47 -0300

    Consertar ordem dos posts sem foto

--- js/Post.js
+++ js/Post.js
@@ -1,35 +1,61 @@
 class Post {
   constructor(imgPath, userImgPath, nomeUsuario, legenda) {
-    this.imgPath = imgPath;
+    this.imgPath = imgPath || null;
     this.userImgPath = userImgPath || null;
     this.nomeUsuario = nomeUsuario;
     this.legenda = legenda;
   }
 
-  constroiElemento() {
+  constroiHeader() {
     return `
           <div class="post_header f_align_center">
             <img src="${this.userImgPath}">
             <span>${this.nomeUsuario}</span>
           </div>
-          <div class="post_conteudo">
-            <div class="post_imagem">
-              <img src="${this.imgPath}" alt="Imagem do post">
-            </div>
-            <div class="post_btns espacado">
-              <span class="interact">
-                <iconify-icon icon="ant-design:like-outlined" width="32" height="32"></iconify-icon>
-                <iconify-icon icon="uiw:message" width="32" height="32"></iconify-icon>
-              </span>
-              <span class="like">
-                <iconify-icon icon="icon-park-outline:like" style="color: #db0031;" width="32" height="32"></iconify-icon>
-              </span>
-            </div>
-            <div class="legenda">
-              <p>${this.legenda}</p>
-            </div>
-          </div>
-        `
+    `
+  }
+
+  constroiImagem() {
+    if (this.imgPath) {
+      return `
+        <div class="post_imagem">
+          <img src="${this.imgPath}" alt="Imagem do post">
+        </div>
+      `
+    }
+    return "";
+  }
+
+  constroiBtns() {
+    return `
+      <div class="post_btns espacado">
+        <span class="interact">
+          <iconify-icon icon="ant-design:like-outlined" width="32" height="32"></iconify-icon>
+          <iconify-icon icon="uiw:message" width="32" height="32"></iconify-icon>
+        </span>
+        <span class="like">
+          <iconify-icon icon="icon-park-outline:like" style="color: #db0031;" width="32" height="32"></iconify-icon>
+        </span>
+      </div>
+    `
+  }
+  constroiLegenda() {
+    return `
+      <div class="legenda">
+        <p>${this.legenda}</p>
+      </div>
+    `
+  }
+
+  constroiElemento() {
+    let element = this.constroiHeader() + '<div class="post_conteudo">' + this.constroiImagem();
+    if (this.imgPath) {
+      element += this.constroiBtns() + this.constroiLegenda();
+    } else {
+      element += this.constroiLegenda() + this.constroiBtns();
+    }
+    element += "</div>";
+    return element;
   }
 }
 
